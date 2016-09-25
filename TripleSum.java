36f8591fc2dd3cf493b4aa3ad883b32d5925578a
zrdaley <zrdaley@gmail.com>
2016-09-25 16:41:31 -0700

    Update TripleSum.java

--- TripleSum.java
+++ TripleSum.java
@@ -1,3 +1,10 @@
+/*
+Input: An array 𝐴 of 𝑛 non-negative integers.
+
+Output: A boolean value (true or false). If there are three indices 𝑖, 𝑗, 𝑘 such that 0 ≤ 𝑖, 𝑗, 𝑘 ≤
+𝑛 − 1 and 𝐴[𝑖] + 𝐴[𝑗] + 𝐴[𝑘] = 225, the output will be true. If no such indices exist, the output will be false. Note that 𝑖, 𝑗 and 𝑘 do not have to be distinct.
+*/
+
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.Arrays;
